f5620346c2f466e313448bba873698fca56940b1
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-10-13 13:10:47 +0900

    Refactor aggregate_bo_results function to include should_append parameter and update output handling; add speedup calculations for optimization metrics

--- experiments/aggregate_bo_results.py
+++ experiments/aggregate_bo_results.py
@@ -4,7 +4,9 @@ import argparse
 import pandas as pd
 
 
-def aggregate_bo_results(input_file: str, output_file: str) -> None:
+def aggregate_bo_results(
+    input_file: str, output_file: str, should_append: bool
+) -> None:
     with open(input_file, "r") as f:
         df = pd.read_json(f, lines=True)
 
@@ -29,35 +31,55 @@ def aggregate_bo_results(input_file: str, output_file: str) -> None:
             acqf_opt_time_q3=("elapsed_acqf_opt", lambda x: x.quantile(0.75)),
             avg_nits_mean=("n_iter_mean", "mean"),
             avg_nits_std=("n_iter_mean", "std"),
-            avg_nits_median=("n_iter_median", "median"),
-            avg_nits_q1=("n_iter_median", lambda x: x.quantile(0.25)),
-            avg_nits_q3=("n_iter_median", lambda x: x.quantile(0.75)),
+            med_nits_median=("n_iter_median", "median"),
+            med_nits_q1=("n_iter_median", lambda x: x.quantile(0.25)),
+            med_nits_q3=("n_iter_median", lambda x: x.quantile(0.75)),
         )
         .reset_index()
     )
     summary = summary.fillna(0)
 
-    summary["Best Objective Value (mean ± std. dev.)"] = summary.apply(
+    summary["Best Value (mean ± std.)"] = summary.apply(
         lambda row: f"{row['best_value_mean']:.2f} ± {row['best_value_std']:.2f}",
         axis=1,
     )
-    summary["Best Objective Value (median [Q1, Q3])"] = summary.apply(
+    summary["Best Value (median [IQR])"] = summary.apply(
         lambda row: f"{row['best_value_median']:.2f} [{row['best_value_q1']:.2f}, {row['best_value_q3']:.2f}]",
         axis=1,
     )
-    summary["Acq. Func. Opt. Time (s, mean ± std. dev.)"] = summary.apply(
+    summary["Acq. Opt. (sec, mean ± std.)"] = summary.apply(
         lambda row: f"{row['acqf_opt_time_mean']:.2f} ± {row['acqf_opt_time_std']:.2f}",
         axis=1,
     )
-    summary["Acq. Func. Opt. Time (s, median [Q1, Q3])"] = summary.apply(
+    summary["Acq. Opt. (sec, median [IQR])"] = summary.apply(
         lambda row: f"{row['acqf_opt_time_median']:.2f} [{row['acqf_opt_time_q1']:.2f}, {row['acqf_opt_time_q3']:.2f}]",
         axis=1,
     )
-    summary["Avg. L-BFGS Iterations (mean ± std. dev.)"] = summary.apply(
+    summary["Avg. Iters (mean ± std.)"] = summary.apply(
         lambda row: f"{row['avg_nits_mean']:.2f} ± {row['avg_nits_std']:.2f}", axis=1
     )
-    summary["Avg. L-BFGS Iterations (median [Q1, Q3])"] = summary.apply(
-        lambda row: f"{row['avg_nits_median']:.2f} [{row['avg_nits_q1']:.2f}, {row['avg_nits_q3']:.2f}]",
+    summary["Avg. Iters (median [IQR])"] = summary.apply(
+        lambda row: f"{row['med_nits_median']:.2f} [{row['med_nits_q1']:.2f}, {row['med_nits_q3']:.2f}]",
+        axis=1,
+    )
+    # speedup = original_mode_time / proposed_mode_time
+    summary["mean_speedup"] = summary.apply(
+        lambda row: summary[
+            (summary["function_id"] == row["function_id"])
+            & (summary["dimension"] == row["dimension"])
+            & (summary["mode"] == "original")
+        ]["acqf_opt_time_mean"].values[0]
+        / row["acqf_opt_time_mean"],
+        axis=1,
+    )
+
+    summary["median_speedup"] = summary.apply(
+        lambda row: summary[
+            (summary["function_id"] == row["function_id"])
+            & (summary["dimension"] == row["dimension"])
+            & (summary["mode"] == "original")
+        ]["acqf_opt_time_median"].values[0]
+        / row["acqf_opt_time_median"],
         axis=1,
     )
 
@@ -66,16 +88,38 @@ def aggregate_bo_results(input_file: str, output_file: str) -> None:
             "function_id",
             "dimension",
             "mode",
-            "Best Objective Value (mean ± std. dev.)",
-            "Best Objective Value (median [Q1, Q3])",
-            "Acq. Func. Opt. Time (s, mean ± std. dev.)",
-            "Acq. Func. Opt. Time (s, median [Q1, Q3])",
-            "Avg. L-BFGS Iterations (mean ± std. dev.)",
-            "Avg. L-BFGS Iterations (median [Q1, Q3])",
+            "best_value_mean",
+            "best_value_std",
+            "best_value_median",
+            "best_value_q1",
+            "best_value_q3",
+            "acqf_opt_time_mean",
+            "acqf_opt_time_std",
+            "acqf_opt_time_median",
+            "acqf_opt_time_q1",
+            "acqf_opt_time_q3",
+            "avg_nits_mean",
+            "avg_nits_std",
+            "med_nits_median",
+            "med_nits_q1",
+            "med_nits_q3",
+            "mean_speedup",
+            "median_speedup",
+            # "Best Value (mean ± std.)",
+            # "Best Value (median [IQR])",
+            # "Acq. Opt. (sec, mean ± std.)",
+            # "Acq. Opt. (sec, median [IQR])",
+            # "Avg. Iters (mean ± std.)",
+            # "Avg. Iters (median [IQR])",
         ]
     ].rename(columns={"mode": "Method"})
 
-    output_df.to_csv(output_file, index=False, encoding="utf-8-sig")
+    output_df.to_csv(
+        output_file,
+        index=False,
+        encoding="utf-8-sig",
+        mode="a" if should_append else "w",
+    )
     print(output_df.to_string(index=False))
 
 
@@ -91,5 +135,9 @@ if __name__ == "__main__":
         default="aggregated_bench_stats.csv",
         help="Output CSV file",
     )
+    parser.add_argument(
+        "--append", action="store_true", help="Append to the output file if it exists"
+    )
     args = parser.parse_args()
-    aggregate_bo_results(args.input, args.output)
+    print("Aggregating BO results...")
+    aggregate_bo_results(args.input, args.output, args.append)
